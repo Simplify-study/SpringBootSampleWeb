9d4a34d37f5707743bc24e6de30ee8d648de23c5
Jonghun <jonghun.criss.android@gmail.com>
2019-06-15 01:15:05 +0900

    for tomcat8

--- src/main/java/com/simplify/sample/SpringBootSampleApplication.java
+++ src/main/java/com/simplify/sample/SpringBootSampleApplication.java
@@ -2,11 +2,28 @@ package com.simplify.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSampleApplication {
+public class SpringBootSampleApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSampleApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootSampleApplication.class);
+	}
+	
+	
 }
+
+//@SpringBootApplication
+//public class SpringBootSampleApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringBootSampleApplication.class, args);
+//	}
+//}
